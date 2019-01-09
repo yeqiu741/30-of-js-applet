9b0e8af5971de969d72ccaedfbe4688f3144e208
yeqiu741 <1428114973@qq.com>
2019-01-09 15:18:22 +0800

    clock钟表小程序的完成

---
+++ js css clock/index.js
@@ -0,0 +1,12 @@
+setInterval(()=>{
+  let time = new Date();
+  const second = time.getSeconds()*6;
+  const minute = time.getMinutes()*6;
+  const hour = time.getHours()/2*30;
+  const secondDom = document.getElementById('second');
+  const minuteDom = document.getElementById('minute');
+  const hourDom = document.getElementById('hour');
+  secondDom.style.transform = `rotate(${second}deg)`;
+  minuteDom.style.transform = `rotate(${minute}deg)`;
+  hourDom.style.transform = `rotate(${hour}deg)`;
+},1000)
