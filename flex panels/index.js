35e39d0c8d7b3bb8d67e8dfcc9b79e5371925094
yeqiu741 <1428114973@qq.com>
2019-01-14 12:34:16 +0800

    修复了一些小问题

--- flex panels/index.js
+++ flex panels/index.js
@@ -35,47 +35,55 @@ domlist.forEach((item,index) => {
   const divDom = document.createElement('div');
   divDom.style.backgroundImage = `url(${item.imgUrl})`;
   divDom.style.backgroundSize = 'cover';
-  divDom.style.display = 'inline-block';
   divDom.id = `imgDiv${index}`
   const pTop = document.createElement('p')
   pTop.innerHTML = item.topTitle
-  pTop.style.lineHeight = '282px'
   pTop.className = 'pTop';
   const pCenter = document.createElement('p')
   pCenter.innerHTML = item.centerTitle
-  pCenter.style.lineHeight = '315px'
+  pCenter.style.lineHeight = '866px'
   const pBottom = document.createElement('p')
   pBottom.innerHTML = item.bottomTitle
-  pBottom.style.lineHeight = '282px'
   pBottom.className = 'pBottom';
   divDom.appendChild(pTop);
   divDom.appendChild(pCenter);
   divDom.appendChild(pBottom);
   container.appendChild(divDom);
 })
-const nodelist = document.querySelectorAll('div')
-console.log(nodelist)
+const nodelist = container.querySelectorAll('div')
 nodelist.forEach(item => {
   item.addEventListener('click', transform)
 })
-let i = 0;
+const idArray = [1, 2, 3, 4]
 function transform(e){
   // 阻止事件冒泡
   e.stopPropagation(); 
-
-  console.log(this.id)
-  console.log(i)
-  if(i++%2== 0 || this.style.width == ''){
-
-    this.style.width = '40%'
-    this.style.fontSize = '25px';
-    this.children[0].style.opacity = 1
-  this.children[2].style.opacity = 1
+  if(parseInt(this.style.fontSize)%2== 0 || this.style.width == ''){
+    let fontSize = 20
+    let opacity = 0
+    let width = 12
+    let margin = 0
+    const a = setInterval(() => {
+      if(fontSize === 25){
+        clearInterval(a)
+        console.log('测试')
+      }else{
+        fontSize +=1
+        opacity += 0.2
+        width += 5
+        margin += 30
+        this.style.fontSize = fontSize+'px'
+        this.children[0].style.opacity = opacity
+        this.children[2].style.opacity = opacity
+        this.style.width = width+'%'
+        this.children[0].style.top = margin+'px'
+        this.children[2].style.bottom = margin+'px'
+      } 
+    },60)
   }else{
     this.style.width = '20%'
     this.style.fontSize = '20px';
     this.children[0].style.opacity = 0
-  this.children[2].style.opacity = 0
+    this.children[2].style.opacity = 0
   }
-  
 }
