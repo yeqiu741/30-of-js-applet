79c56bb16b2b484304e606d9181ba4dc72f0e269
yeqiu741 <1428114973@qq.com>
2019-01-22 20:56:59 +0800

    完成第六个ajax和正则表达式以及css相结合的校项目

--- 6 ajax type ahead/index.js
+++ 6 ajax type ahead/index.js
@@ -1,15 +1,66 @@
-const msglist = [
-  'Beijing', 'Baoding', 'Langfang', 'Tangshan', 'Tianjin', 'Yanjiao', 'Liaoning', 'Heilongjiang', 
-  'Chongqing', 'Wulumuqi', 'Kelamayi', 'Shihezi', 'Alaer', 'Tumushuke', 'Wujiaqu', 'Hami', 'Tulufan',
-  'Akesu', 'Lasa', 'Yinchuan', 'Shizuishan', 'Guyuan', 'Zhongwei', 'Qingtongxia', 'Lingwu', 'Huhehaote',
-  'Baotou', 'Wuhai', 'Chifeng', 'Tongliao', 'Eerduosi', 'Hulunbeier', 'Bayanzhuoer', 'Wulanchabu', 'Fengzhen',
-  'Nanning', 'Liuzhou', 'Guilin', 'Wuzhou', 'Beihai', 'Hezhou', 'Yulin', 'Baise', 'Hechi', 'Qinzhou', 'Guiping',
-  'Shijiazhuang', 'Handan', 'Qinhuangdao', 'Cangzhou', 'Hengshui', 'Xingtai', 'Zhengzhou', 'Luoyang', 'Kaifeng',
-  'Luohe', 'Anyang', 'Bozhou', 'Hefei', 'Lanzhou', 'Wuxi', 'Huangshan', 'Fuyang'
+let msglist = [
+  'Filters For A City',
+  'Or A State'
 ]
-const datalistDom = document.querySelector('#dataArray')
-msglist.forEach(item => {
-  const optionDom = document.createElement('option');
-  optionDom.value = item;
-  datalistDom.appendChild(optionDom);
+const xhr = new XMLHttpRequest();
+xhr.open('get', './city.json');
+xhr.send();
+xhr.onreadystatechange = () => {
+  if(xhr.readyState == 4 && xhr.status == 200){
+    // console.log(msglist)
+    msglist = JSON.parse(xhr.responseText)
+    // console.log(msglist)
+  }
+}
+const ulDom = document.querySelector('ul')
+
+msglist.forEach((item,index) => {
+  const liDom = document.createElement('li');
+  liDom.innerText = item;
+  if(index%2 == 0)
+  {
+    liDom.className = 'li1'
+  }else{
+    liDom.className = 'li2'
+  }
+  ulDom.appendChild(liDom);
 })
+
+const inputDom = document.querySelector('input')
+inputDom.focus()
+inputDom.select()
+inputDom.addEventListener('input', filterMsg)
+function filterMsg(){
+  const nodelist1 = document.querySelectorAll('li');
+  const liDomArr1 = Array.from(nodelist1)
+  liDomArr1.forEach(item12 => {
+    ulDom.removeChild(item12)
+  })
+  const inputValue = inputDom.value
+  msglist.filter(item => {
+    const filter = new RegExp(`${inputValue}`)
+    if(filter.test(item.city.toLowerCase())){
+      const liDom = document.createElement('li');
+      liDom.innerText = item.city;
+      const nodelist = document.querySelectorAll('li');
+      const liDomArr = Array.from(nodelist)
+      console.log(liDomArr.length)
+      const flog = liDomArr.length+1
+      if(flog%2 != 0){
+        liDom.className = 'li2'
+      }else{
+        liDom.className = 'li1'
+      }
+      ulDom.appendChild(liDom);
+    }else{
+      const nodelist = document.querySelectorAll('li');
+      const liDomArr = Array.from(nodelist)
+      liDomArr.forEach(item1 => {
+        if(item1.innerText == item.city){
+          ulDom.removeChild(item1)
+        }
+      })
+    }
+  })
+}
+
