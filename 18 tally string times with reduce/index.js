0352f2fc98f9ecb7a710305f8695eb535a0d8d78
yeqiu741 <1428114973@qq.com>
2019-01-28 20:16:13 +0800

    完善了一下对比

--- 18 tally string times with reduce/index.js
+++ 18 tally string times with reduce/index.js
@@ -4,3 +4,18 @@ const arr = [
 console.log(arr.map(parseFloat))
 const ulDom = document.querySelector('ul')
 ulDom.innerHTML = arr.map(item => `<li>video${parseFloat(item)}</li>`).join('')
+
+// es5
+// <!-- ES5 -->
+// ['10', '5'].map(function(str) {
+//     return parseFloat(str);
+// });
+
+
+// <!-- ES6 -->
+// ['10', '5'].map(str => parseFloat(str));
+
+
+// <!-- ES6 shorter -->
+// ['10', '5'].map(parseFloat);
+
